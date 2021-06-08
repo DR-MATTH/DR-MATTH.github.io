1703c9b7417b2b12094cc844198ab12b7361b2be
DR-MATTH <mmarinv@est.emi.edu.bo>
2021-06-07 22:30:02 -0400

    _

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
-let main = document.getElementById('main')
-let altura = main.offsetTop
-window.addEventListener('scroll', () => {
-  if (window.pageYOffset > altura)
-    main.classList.add('stick')
-  else 
-    main.classList.remove('stick')
+const toBase64 = file => new Promise((resolve, reject) => {
+  const reader = new FileReader()
+  reader.readAsDataURL(file)
+  reader.onload = () => resolve(reader.result)
+  reader.onerror = error => reject(error)
 })
 
-let alto = document.getElementById('alto')
-let ancho = document.getElementById('ancho')
+async function handleArchivo(e) {
+  const file = e.target.files[0]
+  toBase64(file)
+    .then(base64 => {
+      console.log(base64)
+      fetch(`https://api.imgbb.com/1/upload?key=96966ef8138bc841dfcf76c97b15aea0&image=${base64}&expiration=600`, {
+        method: 'POST',
+      })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(err => console.log(err))
+    })
+    .catch(e => console.log(e))
 
-alto.textContent = window.innerHeight
-ancho.textContent = window.innerWidth
+}
 
-window.addEventListener('resize', () => {
-  alto.textContent = window.innerHeight
-  ancho.textContent = window.innerWidth
-})
+const btnArchivo = document.getElementById('file')
+btnArchivo.addEventListener('change', handleArchivo)
